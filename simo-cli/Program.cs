e1c0fd38622e182a54a348ed511c122d018efaed
bulb <simone.coppi@outlook.it>
2020-01-21 23:42:18 +0100

    * Program.cs: First Commit
    * simo-cli.csproj:

---
+++ simo-cli/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace simo_cli
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var cmd = new RootCommand();
+            cmd.AddCommand(Helloworld());
+            cmd.AddCommand(SalutaConNome());
+            return await cmd.InvokeAsync(args);
+        }
+
+        private static Command Helloworld()
+        {
+            var cmd = new Command("saluta", "Saluta l'interlocutore");
+            cmd.Handler = CommandHandler.Create(() =>
+            {
+                Console.WriteLine("Ciao");
+            });
+            return cmd;
+        }
+
+        private static Command SalutaConNome()
+        {
+            var cmd = new Command("salutawname", "Saluta l'interlocutore chiamandolo per nome");
+            cmd.AddOption(new Option(new[] { "--name", "n" }, "Salutare con il nome")
+            {
+                Argument = new Argument<String>
+                {
+                    Arity = ArgumentArity.ExactlyOne
+                }
+            });
+            cmd.Handler = CommandHandler.Create<string>((name) =>
+            {
+                Console.WriteLine($"Ciao {name}");
+            });
+            return cmd;
+        }
+    }
+}
